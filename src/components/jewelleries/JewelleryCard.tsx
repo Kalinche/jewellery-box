7e4868381ce5d109b1560905f00d065ba55abf02
Kalinche <kalina1406@gmail.com>
2023-10-01 13:51:45 +0300

    Add multiple photo pvisualization in the jewellery card

--- src/components/jewelleries/JewelleryCard.tsx
+++ src/components/jewelleries/JewelleryCard.tsx
@@ -1,28 +1,49 @@
+import { useState } from 'react';
 import { IdentifiableJewellery } from '../../model/jewellery.model';
 import '../styles/Card.css';
 
 const JewelleryCard = ({ jewellery }: { jewellery: IdentifiableJewellery }) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const handlePrevImage = () => {
+        setCurrentImageIndex(prevIndex => (prevIndex - 1 + jewellery.photoUrls?.length) % (jewellery.photoUrls?.length || 1));
+    };
+
+    const handleNextImage = () => {
+        setCurrentImageIndex(prevIndex => (prevIndex + 1) % (jewellery.photoUrls?.length || 1));
+    };
+
     return (
         <div className="card mb-3">
             <div className="card-row">
                 <div className="card-body">
                     <h5 className="card-title">{jewellery.name || `Serial Number: ${jewellery.serialNumber}`}</h5>
-                    <p className="card-description">{jewellery.description}</p>
+                    {jewellery.description && <p className="card-description">{jewellery.description}</p>}
                     <div className="card-details-img">
                         <div className="card-image-col">
-                            <img
-                                src={jewellery.photoUrls?.[0] || 'https://t3.ftcdn.net/jpg/03/80/50/90/360_F_380509039_Sxu7EBlre1HeJGJ02aihDbzWHMlqCoSV.jpg'}
-                                alt={jewellery.name || `Serial Number: ${jewellery.serialNumber}`}
-                                className="card-image"
-                            />
+                            <div className="image-container">
+                                <img
+                                    src={jewellery.photoUrls?.[currentImageIndex] || 'default_image_url'}
+                                    alt={jewellery.name || `Serial Number: ${jewellery.serialNumber}`}
+                                    className="card-image"
+                                />
+                                <div className="buttons-container">
+                                    {jewellery.photoUrls && jewellery.photoUrls.length > 1 && (
+                                        <>
+                                            <button onClick={handlePrevImage}><i>{'<'}</i></button>
+                                            <button onClick={handleNextImage}><i>{'>'}</i></button>
+                                        </>
+                                    )}
+                                </div>
+                            </div>
                         </div>
                         <div className="card-content-col">
                             <ul className="card-details">
-                                <li className="card-list-item">Type: {jewellery.type}</li>
-                                <li className="card-list-item">Collection: {jewellery.collection}</li>
-                                <li className="card-list-item">Style: {jewellery.style}</li>
-                                <li className="card-list-item">Gender: {jewellery.gender}</li>
-                                <li className="card-list-item">Crafting Time: {jewellery.craftingTime} mins</li>
+                                {jewellery.type && <li className="card-list-item">Type: {jewellery.type}</li>}
+                                {jewellery.collection && <li className="card-list-item">Collection: {jewellery.collection}</li>}
+                                {jewellery.style && <li className="card-list-item">Style: {jewellery.style}</li>}
+                                {jewellery.gender && <li className="card-list-item">Gender: {jewellery.gender}</li>}
+                                {jewellery.craftingTime && <li className="card-list-item">Crafting Time: {jewellery.craftingTime} mins</li>}
                             </ul>
                         </div>
                     </div>
